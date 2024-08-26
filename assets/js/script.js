1cca992b703d77c0d1d7398286afec261abe39db
yuminachie <ayumi.nathalie@gmail.com>
2024-08-26 18:28:33 +0000

    save 26/08

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,31 @@ document.querySelector('.menu-opener').addEventListener('click', ()=>{
     let nav=document.querySelector('header nav');
     nav.classList.toggle('opened');
 });
+
+//faz o slider dos testemunhos
+const toggleSlider = () =>{
+    //pega os slides da tela
+    const slides = document.querySelectorAll('input[name=slider]');
+    if(slides.length===0) return;
+    //verifica qual está marcado. Caso nenhum, marca o primeiro
+    let current = document.querySelector('input[name=slider]:checked');
+    if(!current){any
+        current.querySelector('input[name=slider]');
+        current.setAttribute('checked', true);
+    }
+
+    //pega o id do slider e muda de string para inteiro
+let id = parseInt(current.getAttribute('id').split('-')[1])
+if(id+1 <=slides.length){
+    id++;
+}else{
+    id=1;
+}
+
+//pega o slide atual marca e desmarca, alterando entre eles
+for(let slider of slides){
+    slider.removeAttribute('checked');
+}
+document.querySelector('#slider-${id}').setAttribute('checked',true);
+}
+setInterval(toggleSlider, 5000);
